c358b3086f86f6959b452c9b5a318f5080c53537
Adam Robbie <adam.robbie@gmail.com>
2020-09-30 14:14:16 -0400

    add test and mine_block capability

--- lib/blockchain/block.ex
+++ lib/blockchain/block.ex
@@ -1,4 +1,4 @@
-defmodule ExChain.BlockChain.Block do
+defmodule ExChain.Blockchain.Block do
   @moduledoc """
   This module is the single block struct in a blockchain
   """
@@ -11,5 +11,27 @@ defmodule ExChain.BlockChain.Block do
     data: any()
   }
 
-  defstruct ~w(timestramp last_hash hash data)
+  defstruct ~w(timestamp last_hash hash data)a
+
+  @spec new(pos_integer(), String.t(), any()) :: Block.t()
+  def new(timestamp, last_hash, data) do
+    hash = hash(timestamp, last_hash, data)
+    %__MODULE__{timestamp: timestamp, last_hash: last_hash, hash: hash, data: data}
+  end
+
+  @spec genesis() :: Block.t()
+  def genesis() do
+    __MODULE__.new(1_599_909_623_805_627, "-", "genesis data")
+  end
+
+  def mine_block(%__MODULE__{hash: last_hash}, data) do
+    __MODULE__.new(get_timestamp(), last_hash, data)
+  end
+
+  defp get_timestamp(), do: DateTime.utc_now() |> DateTime.to_unix(1_000_000)
+
+  defp hash(timestamp, last_hash, data) do
+    data = "#{timestamp}:#{last_hash}:#{Jason.encode!(data)}"
+    Base.encode16(:crypto.hash(:sha256, data))
+  end
 end
--- test/blockchain/block_test.exs
+++ test/blockchain/block_test.exs
@@ -2,14 +2,14 @@ defmodule ExChain.Blockchain.BlockTest do
   @moduledoc """
   This module contains test related to a block
   """
-  use ExUnit.TestCase
+  use ExUnit. Case
   alias ExChain.Blockchain.Block
 
   describe "block" do
     test ".genesis is valid" do
       assert %Block{
         data: "genesis data",
-        hash: "genesis-hash",
+        hash: "F277BF9150CD035D55BA5B48CB5BCBE8E564B134E5AD0D56E439DD04A1528D3B",
         last_hash: "-",
         timestamp: 1_599_909_623_805_627
       } == Block.genesis()
@@ -18,10 +18,20 @@ defmodule ExChain.Blockchain.BlockTest do
     test ".new gives a new block when we pass in params" do
       timestamp = DateTime.utc_now() |> DateTime.to_unix(1_000_000)
       data = "this is new block data"
-      last_hash = "random_hash_last_block"
+      last_hash = "-"
+      # hash = "random_hash_last_block"
 
-      assert %Block{timestamp: ^timestamp, hash: hash, last_hash: ^last_hash, data: ^data} =
-               Block.new(timestamp, hash, last_hash, data)
+      assert %Block{timestamp: ^timestamp, hash: _hash, last_hash: ^last_hash, data: ^data} =
+               Block.new(timestamp, last_hash, data)
     end
+  end
+
+  test ".mine_block returns a new block" do
+    %Block{hash: hash} = genesis_block = Block.genesis()
+
+    assert %Block{
+      data: "this is mined data",
+      last_hash: ^hash
+    } = Block.mine_block(genesis_block, "this is mined data")
   end
 end
