8e9e514c539d6f9f99ec0c0c488e004bc9404961
Adam Robbie <adam.robbie@gmail.com>
2020-09-30 14:41:11 -0400

    a little refactoring

--- lib/blockchain/block.ex
+++ lib/blockchain/block.ex
@@ -15,8 +15,22 @@ defmodule ExChain.Blockchain.Block do
 
   @spec new(pos_integer(), String.t(), any()) :: Block.t()
   def new(timestamp, last_hash, data) do
-    hash = hash(timestamp, last_hash, data)
-    %__MODULE__{timestamp: timestamp, last_hash: last_hash, hash: hash, data: data}
+    %__MODULE__{}
+    |> add_timestamp(timestamp)
+    |> add_last_hash(last_hash)
+    |> add_data(data)
+    |> add_hash()
+  end
+
+  @spec get_str(Block.t()) :: String.t()
+  def get_str(block = %__MODULE__{}) do
+    """
+    Block
+    timestamp: #{block.timestamp}
+    last_hash: #{block.last_hash}
+    hash: #{block.hash}
+    data: #{block.data}
+    """
   end
 
   @spec genesis() :: Block.t()
@@ -28,7 +42,18 @@ defmodule ExChain.Blockchain.Block do
     __MODULE__.new(get_timestamp(), last_hash, data)
   end
 
+  # private functions
   defp get_timestamp(), do: DateTime.utc_now() |> DateTime.to_unix(1_000_000)
+
+  defp add_timestamp(%__MODULE__{} = block, timestamp), do: %{block | timestamp: timestamp}
+
+  defp add_last_hash(%__MODULE__{} = block, last_hash), do: %{block | last_hash: last_hash}
+
+  defp add_data(%__MODULE__{} = block, data), do: %{block | data: data}
+
+  def add_hash(%__MODULE__{timestamp: timestamp, last_hash: last_hash, data: data} = block) do
+    %{block | hash: hash(timestamp, last_hash, data)}
+  end
 
   defp hash(timestamp, last_hash, data) do
     data = "#{timestamp}:#{last_hash}:#{Jason.encode!(data)}"
